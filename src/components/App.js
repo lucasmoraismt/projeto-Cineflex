15a16e2c19556f582ff9d484c8a99fd3841073ff
Lucas de Morais <lucasmoraismt@gmail.com>
2021-05-10 22:07:26 -0300

    Home page styles + favicon added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,34 @@
+import "../css/reset.css";
+import "../css/styles.css";
+
 export default function App() {
     return (
         <>
             <div className="header">
                 <h1>CINEFLEX</h1>
+            </div>
+            <div className="app">
+                <p className="page-title">Selecione o Filme</p>
+                <ul className="films-list">
+                    <li className="film">
+                        <img src="" alt="" />
+                    </li>
+                    <li className="film">
+                        <img src="" alt="" />
+                    </li>
+                    <li className="film">
+                        <img src="" alt="" />
+                    </li>
+                    <li className="film">
+                        <img src="" alt="" />
+                    </li>
+                    <li className="film">
+                        <img src="" alt="" />
+                    </li>
+                    <li className="film">
+                        <img src="" alt="" />
+                    </li>
+                </ul>
             </div>
         </>
     );
